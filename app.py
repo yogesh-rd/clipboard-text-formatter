9679f56e78420c33ab9011ae0f158a48958c3a8c
yogesh-rd <yogesh1122554@gmail.com>
2022-02-25 16:40:43 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title("Newline character remover")
+icon = tk.PhotoImage(file = "/media/data/projects/remove_newline/icon.png")
+root.iconphoto(False, icon)
+
+def remove_newline_characters():
+    clipboard = root.clipboard_get()
+    root.clipboard_clear()
+    root.clipboard_append(clipboard.replace('\n', ' '))
+
+submit_button = tk.Button(root, text="Remove newline characters", command=remove_newline_characters)
+exit_button = tk.Button(root, text="Exit", command=root.destroy)
+
+submit_button.grid(row=0, column=0)
+exit_button.grid(row=1, column=0)
+
+root.mainloop()
