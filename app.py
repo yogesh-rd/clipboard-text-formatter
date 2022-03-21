53d505e4c271e5df75fca04a7e79698534d42f60
yogesh-rd <yogesh1122554@gmail.com>
2022-03-21 10:46:28 +0530

    add remove bullet points feature

--- app.py
+++ app.py
@@ -6,15 +6,23 @@ root.title("Newline character remover")
 icon = tk.PhotoImage(file="/media/data/projects/remove_newline/icon.png")
 root.iconphoto(False, icon)
 
-def remove_newline_characters():
+def replace_character(chr, replacement):
     clipboard = root.clipboard_get()
     root.clipboard_clear()
-    root.clipboard_append(clipboard.replace('\n', ' '))
+    root.clipboard_append(clipboard.replace(chr, replacement))
 
-submit_button = tk.Button(root, text="Remove newline characters", command=remove_newline_characters)
+def remove_newline_characters():
+    replace_character('\n', ' ')
+
+def remove_bullet_points():
+    replace_character('•', '')
+
+remove_newline_button = tk.Button(root, text="Remove newline characters", command=remove_newline_characters)
+remove_bullets_button = tk.Button(root, text="Remove bullet points", command=remove_bullet_points)
 exit_button = tk.Button(root, text="Exit", command=root.destroy)
 
-submit_button.grid(row=0, column=0)
+remove_newline_button.grid(row=0, column=0)
+remove_bullets_button.grid(row=0, column=1)
 exit_button.grid(row=1, column=0)
 
 root.mainloop()
